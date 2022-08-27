27774b61024395ae3a05669561072f7ff203b098
Joyce <1983276@cegepmontpetit.ca>
2022-08-27 17:05:52 -0400

    Composition du dossier Repository
    UnitOfWork, Repository et interfaces

--- Jungle/Jungle_DataAccess/Data/JungleDbContext.cs
+++ Jungle/Jungle_DataAccess/Data/JungleDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,20 @@ namespace Jungle_DataAccess.Data
     /*public*/ internal class JungleDbContext : DbContext
     {
         public JungleDbContext(DbContextOptions<JungleDbContext> options) : base(options)
-
         {
 
         }
+
+        //public DbSet<Country> Country { get; set; }
+
+        //public DbSet<Destination> Destination { get; set; }
+
+        //public DbSet<Guide> Guide { get; set; }
+
+        //public DbSet<Travel> Travel { get; set; }
+
+        //public DbSet<TravelRecommendation> TravelRecommendation { get; set; }
+
     }
+    
 }
